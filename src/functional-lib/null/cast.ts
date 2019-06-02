2f843bc9d4483481419fe69900081346c5bfae39
anyhowstep <anyhowstep@hotmail.com>
2019-06-02 01:12:33 -0400

    Added null tests

--- src/functional-lib/null/cast.ts
+++ src/functional-lib/null/cast.ts
@@ -1,6 +1,5 @@
 import {SafeMapper} from "../../mapper";
 import {toTypeStr} from "../../type-util";
-import {mapper} from "../../mapper";
 import {MappableInput} from "../../mapper";
 import {ExpectedInput} from "../../mapper";
 import {cast} from "../operator";
@@ -8,18 +7,18 @@ import {literal} from "../literal";
 import {null as nil} from "./null";
 import {match} from "../string";
 
-export function undefinedToNull () {
-    return mapper<
-        SafeMapper<null> &
-        ExpectedInput<null> &
-        MappableInput<null|undefined>
-    >((name : string, mixed : unknown) : null => {
+export function undefinedToNull () : (
+    & SafeMapper<null>
+    & ExpectedInput<null>
+    & MappableInput<null|undefined>
+) {
+    return (name : string, mixed : unknown) : null => {
         //We do not mind === here.
         if (mixed === undefined || mixed === null) {
             return null;
         }
         throw new Error(`${name} must be null|undefined; received ${toTypeStr(mixed)}`);
-    });
+    };
 }
 
 export function emptyStringToNull () {
