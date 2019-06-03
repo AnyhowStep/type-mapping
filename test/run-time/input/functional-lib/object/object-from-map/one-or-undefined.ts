6a888eaf937889a1f80833a8d7eb10bf3d871fa3
anyhowstep <anyhowstep@hotmail.com>
2019-06-03 18:46:38 -0400

    Added basic object-from-map tests

---
+++ test/run-time/input/functional-lib/object/object-from-map/one-or-undefined.ts
@@ -0,0 +1,39 @@
+import * as tape from "tape";
+import * as tm from "../../../../../../dist";
+
+tape(__filename, t => {
+    const f = tm.objectFromMap({
+        a : tm.orUndefined(tm.finiteNumber()),
+        b : tm.literal("a", "b"),
+    });
+
+    t.deepEqual(f("x", { a : 2, b : "a" }), { a : 2, b : "a" });
+    t.deepEqual(f("x", { a : undefined, b : "b" }), { a : undefined, b : "b" });
+    t.false(tm.tryMap(f, "x", { a : "2", b : "a" } as any).success);
+    t.false(tm.tryMap(f, "x", { a : 2, b : "c" } as any).success);
+    t.false(tm.tryMap(f, "x", { a : "2", b : "c" } as any).success);
+    t.false(tm.tryMap(f, "x", { a : 2 } as any).success);
+    t.false(tm.tryMap(f, "x", { a : undefined } as any).success);
+    t.false(tm.tryMap(f, "x", { b : "a" } as any).success);
+
+    t.false(tm.tryMap(f, "x", null as any).success);
+    t.false(tm.tryMap(f, "x", undefined as any).success);
+    t.false(tm.tryMap(f, "x", BigInt(0) as any).success);
+    t.false(tm.tryMap(f, "x", BigInt(1) as any).success);
+    t.false(tm.tryMap(f, "x", new Date() as any).success);
+    t.false(tm.tryMap(f, "x", [] as any).success);
+    t.false(tm.tryMap(f, "x", [true] as any).success);
+    t.false(tm.tryMap(f, "x", function () {} as any).success);
+    t.false(tm.tryMap(f, "x", NaN as any).success);
+    t.false(tm.tryMap(f, "x", -Infinity as any).success);
+    t.false(tm.tryMap(f, "x", +Infinity as any).success);
+    t.false(tm.tryMap(f, "x", "test" as any).success);
+    t.false(tm.tryMap(f, "x", "1" as any).success);
+    t.false(tm.tryMap(f, "x", 0 as any).success);
+    t.false(tm.tryMap(f, "x", 1 as any).success);
+    t.false(tm.tryMap(f, "x", true as any).success);
+    t.false(tm.tryMap(f, "x", false as any).success);
+    t.false(tm.tryMap(f, "x", new Number(1) as any).success);
+
+    t.end();
+});
