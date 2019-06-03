8e8618936762aa7e596e3972ceb71b29cb2750b7
anyhowstep <anyhowstep@hotmail.com>
2019-06-03 18:57:31 -0400

    Added basic pojo tests

---
+++ test/run-time/input/functional-lib/object/pojo/basic.ts
@@ -0,0 +1,35 @@
+import * as tape from "tape";
+import * as tm from "../../../../../../dist";
+
+tape(__filename, t => {
+    const f = tm.pojo();
+
+    const obj = { x : { notAPojo : new Date() } };
+    t.true(f("x", obj) == obj);
+    /**
+        TODO Maybe implement `deepPojo()` or something similar?
+    */
+    t.false(tm.tryMap(f, "x", obj.x.notAPojo as any).success);
+
+    t.false(tm.tryMap(f, "x", null as any).success);
+    t.false(tm.tryMap(f, "x", undefined as any).success);
+    t.false(tm.tryMap(f, "x", BigInt(0) as any).success);
+    t.false(tm.tryMap(f, "x", BigInt(1) as any).success);
+    t.false(tm.tryMap(f, "x", new Date() as any).success);
+    t.false(tm.tryMap(f, "x", [] as any).success);
+    t.false(tm.tryMap(f, "x", [true] as any).success);
+    t.false(tm.tryMap(f, "x", function () {} as any).success);
+    t.false(tm.tryMap(f, "x", NaN as any).success);
+    t.false(tm.tryMap(f, "x", -Infinity as any).success);
+    t.false(tm.tryMap(f, "x", +Infinity as any).success);
+    t.false(tm.tryMap(f, "x", "test" as any).success);
+    t.false(tm.tryMap(f, "x", "1" as any).success);
+    t.false(tm.tryMap(f, "x", 0 as any).success);
+    t.false(tm.tryMap(f, "x", 1 as any).success);
+    t.false(tm.tryMap(f, "x", true as any).success);
+    t.false(tm.tryMap(f, "x", false as any).success);
+    t.false(tm.tryMap(f, "x", new Number(1) as any).success);
+    t.false(tm.tryMap(f, "x", Buffer.from("test-buffer") as any).success);
+
+    t.end();
+});
