a8bd1819df6ba4e4c4da896af459369a08fd9d28
anyhowstep <anyhowstep@hotmail.com>
2019-06-03 21:29:56 -0400

    Added basic rename tests

---
+++ test/run-time/input/functional-lib/object/rename/basic.ts
@@ -0,0 +1,69 @@
+import * as tape from "tape";
+import * as tm from "../../../../../../dist";
+
+tape(__filename, t => {
+    const f = tm.rename("a", "a2", tm.finiteNumber());
+
+    t.deepEqual(f("x", { a : 2 }), { a2 : 2 });
+    t.deepEqual(f("x", { a : 2, a2 : 3 }), { a2 : 3 });
+    t.deepEqual(f("x", { a2 : 4 }), { a2 : 4 });
+    t.false(tm.tryMap(f, "x", { a : "2" } as any).success);
+
+    t.false(tm.tryMap(f, "x", null as any).success);
+    t.false(tm.tryMap(f, "x", undefined as any).success);
+    t.false(tm.tryMap(f, "x", BigInt(0) as any).success);
+    t.false(tm.tryMap(f, "x", BigInt(1) as any).success);
+    t.false(tm.tryMap(f, "x", new Date() as any).success);
+    t.false(tm.tryMap(f, "x", [] as any).success);
+    t.false(tm.tryMap(f, "x", [true] as any).success);
+    t.false(tm.tryMap(f, "x", function () {} as any).success);
+    t.false(tm.tryMap(f, "x", NaN as any).success);
+    t.false(tm.tryMap(f, "x", -Infinity as any).success);
+    t.false(tm.tryMap(f, "x", +Infinity as any).success);
+    t.false(tm.tryMap(f, "x", "test" as any).success);
+    t.false(tm.tryMap(f, "x", "1" as any).success);
+    t.false(tm.tryMap(f, "x", 0 as any).success);
+    t.false(tm.tryMap(f, "x", 1 as any).success);
+    t.false(tm.tryMap(f, "x", true as any).success);
+    t.false(tm.tryMap(f, "x", false as any).success);
+    t.false(tm.tryMap(f, "x", new Number(1) as any).success);
+
+    t.end();
+});
+
+tape(__filename, t => {
+    const f = tm.rename("a", "a2", tm.orUndefined(tm.finiteNumber()));
+
+    t.deepEqual(f("x", { a : 2 }), { a2 : 2 });
+    t.deepEqual(f("x", { a : 2, a2 : 3 }), { a2 : 3 });
+    t.deepEqual(f("x", { a2 : 4 }), { a2 : 4 });
+    t.false(tm.tryMap(f, "x", { a : "2" } as any).success);
+
+    t.deepEqual(f("x", { a : undefined }), { a2 : undefined });
+    t.deepEqual(f("x", { a : undefined, a2 : 3 }), { a2 : 3 });
+    t.deepEqual(f("x", { a : 5, a2 : undefined }), { a2 : undefined });
+    t.deepEqual(f("x", { a2 : 2 }), { a2 : 2 });
+    t.deepEqual(f("x", { a2 : undefined }), { a2 : undefined });
+    t.false(tm.tryMap(f, "x", { a : "2" } as any).success);
+
+    t.false(tm.tryMap(f, "x", null as any).success);
+    t.false(tm.tryMap(f, "x", undefined as any).success);
+    t.false(tm.tryMap(f, "x", BigInt(0) as any).success);
+    t.false(tm.tryMap(f, "x", BigInt(1) as any).success);
+    t.false(tm.tryMap(f, "x", new Date() as any).success);
+    t.false(tm.tryMap(f, "x", [] as any).success);
+    t.false(tm.tryMap(f, "x", [true] as any).success);
+    t.false(tm.tryMap(f, "x", function () {} as any).success);
+    t.false(tm.tryMap(f, "x", NaN as any).success);
+    t.false(tm.tryMap(f, "x", -Infinity as any).success);
+    t.false(tm.tryMap(f, "x", +Infinity as any).success);
+    t.false(tm.tryMap(f, "x", "test" as any).success);
+    t.false(tm.tryMap(f, "x", "1" as any).success);
+    t.false(tm.tryMap(f, "x", 0 as any).success);
+    t.false(tm.tryMap(f, "x", 1 as any).success);
+    t.false(tm.tryMap(f, "x", true as any).success);
+    t.false(tm.tryMap(f, "x", false as any).success);
+    t.false(tm.tryMap(f, "x", new Number(1) as any).success);
+
+    t.end();
+});
