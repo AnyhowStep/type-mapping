ea57455de76ad4d161dc6fc9b491c2fe0fda3849
anyhowstep <anyhowstep@hotmail.com>
2019-06-06 05:13:27 -0400

    Added extra prop test

--- test/run-time/input/decorator/prop/basic.ts
+++ test/run-time/input/decorator/prop/basic.ts
@@ -22,3 +22,20 @@ tape(__filename, t => {
 
     t.end();
 });
+tape(__filename, t => {
+    class Clazz {
+        @tm.prop(tm.stringToFiniteNumber())
+        prop0 : number = 1;
+    }
+    const c = new Clazz();
+
+    c.prop0 = 5;
+    t.deepEqual(c.prop0, 5);
+    t.doesNotThrow(() => {
+        c.prop0 = "6" as any;
+    });
+    t.deepEqual(c.prop0, 6);
+    t.deepEqual(JSON.stringify(c), `{"prop0":6}`);
+
+    t.end();
+});
