2e8a9525e61fa8dd433db4daab0794efb869d242
anyhowstep <anyhowstep@hotmail.com>
2019-06-05 22:35:12 -0400

    Added basic array tests

---
+++ test/run-time/input/functional-lib/array/array.ts
@@ -0,0 +1,41 @@
+import * as tape from "tape";
+import * as tm from "../../../../../dist";
+
+tape(__filename, t => {
+    const f = tm.array(tm.stringToFiniteNumber());
+
+    const arr0 : number[] = [];
+    t.true(f("x", arr0) == arr0);
+    const arr1 = [1,2,3];
+    t.true(f("x", arr1) == arr1);
+    const arr2 = [1,2,"3"];
+    t.false(f("x", arr2) == arr2);
+    t.deepEqual(f("x", arr2), [1,2,3]);
+
+
+    t.false(tm.tryMap(f, "x", new Date(NaN) as any).success);
+    t.false(tm.tryMap(f, "x", new Date(-Infinity) as any).success);
+    t.false(tm.tryMap(f, "x", new Date(+Infinity) as any).success);
+    t.false(tm.tryMap(f, "x", new Date("qwerty") as any).success);
+    t.false(tm.tryMap(f, "x", null as any).success);
+    t.false(tm.tryMap(f, "x", BigInt(0) as any).success);
+    t.false(tm.tryMap(f, "x", BigInt(1) as any).success);
+    t.false(tm.tryMap(f, "x", [true] as any).success);
+    t.false(tm.tryMap(f, "x", function () {} as any).success);
+    t.false(tm.tryMap(f, "x", NaN as any).success);
+    t.false(tm.tryMap(f, "x", -Infinity as any).success);
+    t.false(tm.tryMap(f, "x", +Infinity as any).success);
+    t.false(tm.tryMap(f, "x", "test" as any).success);
+    t.false(tm.tryMap(f, "x", "null" as any).success);
+    t.false(tm.tryMap(f, "x", "" as any).success);
+    t.false(tm.tryMap(f, "x", "   " as any).success);
+    t.false(tm.tryMap(f, "x", "{}" as any).success);
+    t.false(tm.tryMap(f, "x", 0 as any).success);
+    t.false(tm.tryMap(f, "x", 1 as any).success);
+    t.false(tm.tryMap(f, "x", 2 as any).success);
+    t.false(tm.tryMap(f, "x", true as any).success);
+    t.false(tm.tryMap(f, "x", false as any).success);
+    t.false(tm.tryMap(f, "x", new Number(1) as any).success);
+
+    t.end();
+});
